014d3ec38b77f81804b9de9360fa06ff8bf3b50e
MatteoVentura7 <matteo.ventura18@gmail.com>
2025-12-15 17:29:17 +0100

    add component

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -6,6 +6,7 @@ import { queryParams } from '@/wayfinder';
 import { Inertia } from '@inertiajs/inertia';
 import { Head, useForm } from '@inertiajs/react';
 import { useState } from 'react';
+import ChartCounter from '../components/chartCounter';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -242,25 +243,8 @@ export default function Dashboard({
                     </div>
 
                     {/* STATISTICHE */}
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <h1 className="mt-5 mb-4 text-center text-3xl font-bold text-blue-500">
-                            Your progress chart
-                        </h1>
-                        <div className="flex justify-center gap-4">
-                            <div className="rounded-lg bg-white p-4 text-center text-lg shadow dark:bg-neutral-800">
-                                <span className="font-semibold">To Do</span>{' '}
-                                <h1 className="mt-3 text-4xl font-bold text-blue-600">
-                                    {statistc[0] || 0}
-                                </h1>
-                            </div>
-                            <div className="rounded-lg bg-white p-4 text-center text-lg shadow dark:bg-neutral-800">
-                                <span className="font-semibold">Done</span>{' '}
-                                <h1 className="mt-3 text-4xl font-bold text-green-600">
-                                    {statistc[1] || 0}
-                                </h1>
-                            </div>
-                        </div>
-                    </div>
+                    <ChartCounter statistc={statistc} />
+
                 </div>
 
                 <div className="relative min-h-screen flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
