0631a746a92093adca078b71e4e50dab1d2e9843
MatteoVentura7 <matteo.ventura18@gmail.com>
2025-12-16 17:33:13 +0100

    add main nav

--- resources/js/components/nav-main.tsx
+++ resources/js/components/nav-main.tsx
@@ -8,6 +8,7 @@ import {
 import { resolveUrl } from '@/lib/utils';
 import { type NavItem } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
+import { welcome } from '@/routes';
 
 export function NavMain({ items = [] }: { items: NavItem[] }) {
     const page = usePage();
@@ -24,10 +25,31 @@ export function NavMain({ items = [] }: { items: NavItem[] }) {
                             )}
                             tooltip={{ children: item.title }}
                         >
-                            <Link href={item.href} prefetch>
-                                {item.icon && <item.icon />}
-                                <span>{item.title}</span>
-                            </Link>
+                            
+                                <Link href={item.href} prefetch>
+                                    {item.icon && <item.icon />}
+                                    <span>{item.title}</span>
+                                </Link>
+                                  
+                        </SidebarMenuButton>
+                        <SidebarMenuButton
+                            asChild
+                            isActive={page.url.startsWith(
+                                resolveUrl(item.href),
+                            )}
+                            tooltip={{ children: item.title }}
+                        >
+                            
+                                
+                                 <Link
+                                    href={welcome()}
+                                    className="inline-block rounded-sm border border-transparent  text-sm leading-normal text-[#1b1b18]  dark:text-[#EDEDEC] mt-3 "
+                                >
+                                    <i className="fa-solid fa-list-check"></i>
+                                    <span className='ml-2'>Activity</span>
+                                </Link> 
+                                
+                            
                         </SidebarMenuButton>
                     </SidebarMenuItem>
                 ))}
--- resources/js/pages/greetings.tsx
+++ resources/js/pages/greetings.tsx
@@ -1,3 +1,26 @@
-export default function Greetings({name}: any) {
-    return <h1>Welcome, {name}</h1>;
+
+import AppLayout from '@/layouts/app-layout';
+import { dashboard } from '@/routes';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/react';
+
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Activity',
+        href: dashboard().url,
+    },
+];
+export default function Greetings() {
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Activity" />
+              <div className="  relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                                        
+                                            <h1>Welcome</h1>
+                                        </div>
+                
+        </AppLayout>
+    )
 }
+
