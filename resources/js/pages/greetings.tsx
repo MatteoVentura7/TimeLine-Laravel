d1449a0bbc5bd7b04a70320b7c9da5917ad24441
MatteoVentura7 <matteo.ventura18@gmail.com>
2025-12-17 10:19:48 +0100

    add page activity

--- resources/js/pages/greetings.tsx
+++ resources/js/pages/greetings.tsx
@@ -1,9 +1,10 @@
-
+import ListItem from '@/components/listItem';
+import TaskForm from '@/components/taskForm';
 import AppLayout from '@/layouts/app-layout';
 import { dashboard } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
-
+import ChartCounter from '../components/chartCounter';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -11,16 +12,40 @@ const breadcrumbs: BreadcrumbItem[] = [
         href: dashboard().url,
     },
 ];
-export default function Greetings() {
+
+interface Task {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+export default function Greetings({
+    tasks = [],
+    statistc,
+}: {
+    tasks: Task[];
+    statistc: number[];
+}) {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Activity" />
-              <div className="  relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                                        
-                                            <h1>Welcome</h1>
-                                        </div>
-                
+            <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                <div className="flex justify-around p-10">
+                    <div className="w-full max-w-150 ">
+                    {/* FORM AGGIUNTA TASK */}
+                    <TaskForm />
+                   
+                </div>
+                 <div>
+                    {/* STATISTICHE */}
+                    <ChartCounter statistc={statistc} />
+                    </div>
+                    </div>
+                <div>
+                    {/* LISTA TASK */}
+                    <ListItem tasks={tasks} />
+                </div>
+            </div>
         </AppLayout>
-    )
+    );
 }
-
--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,17 @@ use Laravel\Fortify\Features;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Auth; 
 
-Route::get('welcome', function (Request $request) {
-    return Inertia::render('greetings', ['name' => $request->get('name')]);
+Route::middleware(['auth', 'verified'])->group(function () {
+Route::get('welcome', function () {
+  
+     $tasks = Auth::user()->tasks;
+     $statistc = Auth::user()->tasks->groupBy->completed->map->count();
+      return Inertia::render('greetings', ['tasks' => $tasks , 'statistc' => $statistc]);
 })->name('welcome');
+Route::post('tasks', [TaskController::class, 'store'])->name('tasks.store');
+    Route::patch('tasks/{task}/toggle', [TaskController::class, 'update'])->name('tasks.toggle');
+    Route::delete('tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+});
 
 
 Route::get('/', function () {
